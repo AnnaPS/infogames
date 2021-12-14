88d21b3eb436f6de7ba2267a032714b19a8596c3
Ana <anna-91_@hotmail.com>
2021-12-14 18:37:43 +0100

    added category list widget

---
+++ lib/ui/home/widgets/category/category_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infogames/repository/models/genre.dart';
+import 'package:infogames/ui/home/widgets/category/category.dart';
+import 'package:infogames/ui/home/widgets/category/category_widget.dart';
+
+typedef CategorySelected = Function(int categoryId);
+
+class CategoryList extends StatelessWidget {
+  const CategoryList({
+    Key? key,
+    required this.callback,
+  }) : super(key: key);
+  final CategorySelected callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CategoryBloc, CategoryState>(
+      builder: (context, state) {
+        return SizedBox(
+          height: MediaQuery.of(context).size.height * .2,
+          child: ListView.separated(
+            key: ValueKey('categoryKey'),
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return CategoryWidget(
+                key: ValueKey('${state.categories[index].name}$index'),
+                category: state.categories[index],
+                color: Colors.deepOrangeAccent,
+                callback: (Genre categorySelected) {
+                  callback(categorySelected.id);
+                  context.read<CategoryBloc>().add(
+                        SelectCategory(
+                          idSelected: categorySelected.id,
+                        ),
+                      );
+                },
+              );
+            },
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (_, __) => SizedBox(
+              width: 16.0,
+            ),
+            itemCount: state.categories.length,
+          ),
+        );
+      },
+    );
+  }
+}
